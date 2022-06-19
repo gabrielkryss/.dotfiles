f30190c3e94112adf3ca56f570832e180251956f
Gabriel Kwong <gabrielkwong971@gmail.com>
2022-06-18 19:46:46 -0700

    Create colorscheme.lua

---
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,16 @@
+-- color scheme selections
+-------------------------
+-- defaukt
+-- gruvbox
+-- nightfox
+-- tokyonight
+-- everforest
+-- bluewery
+
+local colorscheme = "default" -- change color scheme here 
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
