a5fef10abad046a79c4ce5fafa5c098aaf0b877d
Gabriel Kwong <gabrielkwong971@gmail.com>
2022-12-23 05:26:47 -0800

    Update user_profile.ps1

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,5 +1,5 @@
-## NOTE: paste the following line into $PROFILE.CurrentUserCurrentHost (use nvim to open that file)
-## $env:USERPROFILE\.config\powershell\user_profile.ps1
+## NOTE: paste the following line into $PROFILE.CurrentUserCurrentHost (use nvim to open that file), including the dot.
+## . $env:USERPROFILE\.config\powershell\user_profile.ps1
 
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
