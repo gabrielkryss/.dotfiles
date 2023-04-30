68233f58552cbaeda850b21ba206b75205296d57
Gabriel Kwong <gabrielkwong971@gmail.com>
2023-04-29 22:20:04 -0700

    Update harpoon.lua

--- nvim/.config/LazyVim/lua/plugins/harpoon.lua
+++ nvim/.config/LazyVim/lua/plugins/harpoon.lua
@@ -6,12 +6,12 @@ return {
             local ui = require("harpoon.ui")
             
             vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon Mark" })
-            vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon Menu Toggle" })
+            vim.keymap.set("n", "<leader>uh", ui.toggle_quick_menu, { desc = "Harpoon Menu Toggle" })
             
-            -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-            -- vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-            -- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-            -- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+            vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+            vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+            vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+            vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
         end
     },
 }
