f935f6ab1faa037f20a9c1ad6dfebd19c4aa9c30
Gabriel Kwong <gabrielkwong971@gmail.com>
2023-05-13 17:25:23 -0700

    Update options.lua

--- nvim/.config/LazyVim/lua/config/options.lua
+++ nvim/.config/LazyVim/lua/config/options.lua
@@ -64,3 +64,16 @@ vim.opt.updatetime = 50
 
 vim.o.clipboard = "unnamedplus"
 
+-- For WSL Clipboard Note: Comment Out for Unix?
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
