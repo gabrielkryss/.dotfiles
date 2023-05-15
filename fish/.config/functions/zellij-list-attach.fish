f24839dd9f37effee5927f96bf9f58d03b57e366
unknown <gabrielkwong971@gmail.com>
2023-05-15 13:29:47 -0700

    added zellij list and attach to session fish function

---
+++ fish/.config/functions/zellij-list-attach.fish
@@ -0,0 +1,9 @@
+# zellij-attach
+function za
+    if set -l sessions (zellij list-sessions | rg -o '^[^\n]+')
+        set -l selected_session (zellij list-sessions | string split -m 1 '\n' | fzf)
+        if test -n "$selected_session"
+            zellij attach "$selected_session"
+        end
+    end
+end
