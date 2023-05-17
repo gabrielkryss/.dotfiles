62552963058e67226542381e34a76c20225b03b9
Gabriel Kwong <gabrielkwong971@gmail.com>
2023-05-16 21:06:50 -0700

    Update user_profile.ps1

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,6 +1,19 @@
 ## NOTE: paste the following line into $PROFILE.CurrentUserCurrentHost (use nvim to open that file), including the dot.
 ## . $env:USERPROFILE\.config\powershell\user_profile.ps1
 
+## Install Commands
+## Install-Module -Name Terminal-Icons -Repository PSGallery
+## Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+## Install-Module -Name z -Force
+## scoop install fzf
+## scoop install fzf OR Install-Module -Name PSFzf -scope currentUser
+## Install-Module -Name ZLocation -scope currentUser
+## scoop install fd
+## scoop install starship
+## scoop install ripgrep
+## Install-Module posh-git -Scope CurrentUser
+## Install-Module oh-my-posh -Scope CurrentUser
+
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
@@ -27,17 +40,27 @@ $ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
 Import-Module -Name Terminal-Icons
 
 # # PSReadLine
-# Set-PSReadLineOption -EditMode Emacs
+# Set Some Option for PSReadLine to show the history of our typed commands
 # Set-PSReadLineOption -BellStyle None
-# Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-# Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionSource History 
+Set-PSReadLineOption -PredictionViewStyle ListView 
+Set-PSReadLineOption -EditMode Emacs 
+
+# Installation lines:
 
 # # Fzf
-# Import-Module PSFzf
-# Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+Import-Module PSFzf
+Enable-PsFzfAliases
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Chord ctrl+f -ScriptBlock { fd | Invoke-fzf | Set-Location }
+
+# ZLocation
+Import-Module ZLocation
 
 # Aliases
 Set-Alias grep findstr
+Set-Alias vim nvim 
+Set-Alias ll ls 
 
 # Utilities
 function which ($command) {
