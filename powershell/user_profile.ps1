051a72642f8c670623cc04dfcbce953ec22fce43
Gabriel Kwong <gabrielkwong971@gmail.com>
2023-05-16 17:14:35 -0700

    Update user_profile.ps1

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -4,7 +4,7 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-Import-Module posh-git
+# Import-Module posh-git
 # themes link ref: https://ohmyposh.dev/docs/themes
 # - star
 # - spaceship
@@ -18,9 +18,11 @@ Import-Module posh-git
 # - lambdageneration
 # - velvet
 # - xtoys
-$omp_config = Join-Path $PSScriptRoot ".\themes\star.omp.json" #  local oh-my-posh (omp) config file e.g. ".\takuya.omp.json"
+# $omp_config = Join-Path $PSScriptRoot ".\themes\xtoys.omp.json" #  local oh-my-posh (omp) config file e.g. ".\takuya.omp.json"
 # $omp_config = "https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/star.omp.json" # remote omp config
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+$ENV:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
 
 Import-Module -Name Terminal-Icons
 
@@ -41,6 +43,22 @@ Set-Alias grep findstr
 function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+## This doesnt work
+# Get the SystemParametersInfo API function
+$SystemParametersInfo = Add-Type -MemberDefinition @"
+[DllImport("user32.dll", SetLastError = true)]
+public static extern bool SystemParametersInfo(
+  uint uiAction,
+  uint uiParam,
+  string pvParam,
+  uint fWinIni
+);
+"@ -Name "NativeMethods" -Namespace Win32 -PassThru
+# function to change wallapaper
+function cw ($PathToImg) {
+  $SystemParametersInfo::SystemParametersInfo(20, 0, $PathToImg, 0x01)
 }
 
 Invoke-Expression (&starship init powershell)
