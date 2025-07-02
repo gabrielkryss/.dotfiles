b05ae0416b412353de58aec9fbcd6d4d30464b23
Gabriel Kwong <gabrielkwong971@gmail.com>
2025-07-01 23:08:44 -0700

    update options
    made powershell Core the default shell for windows.

--- nvim/.config/LazyVim/lua/config/options.lua
+++ nvim/.config/LazyVim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Detect the operating system and set shell accordingly
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  vim.opt.shell = "pwsh"
+  vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
