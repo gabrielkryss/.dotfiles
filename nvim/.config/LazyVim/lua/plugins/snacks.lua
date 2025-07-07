01b991f445cfc17ca312dfdec94724f562ad1b36
Gabriel Kwong <gabrielkwong971@gmail.com>
2025-07-06 22:01:59 -0700

    added and confitured snacks.lua

---
+++ nvim/.config/LazyVim/lua/plugins/snacks.lua
@@ -0,0 +1,53 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      layouts = {
+        -- this doesn't seem to work
+        my_custom_layout = {
+          backdrop = false,
+          row = 1,
+          width = 0.4,
+          min_width = 80,
+          height = 0.8,
+          border = "none",
+          box = "vertical",
+          { win = "preview", title = "{preview}", height = 0.4, border = "rounded" },
+          {
+            box = "vertical",
+            border = "rounded",
+            title = "{title} {live} {flags}",
+            title_pos = "center",
+            { win = "list", border = "none" },
+            { win = "input", height = 1, border = "bottom" },
+          },
+        },
+      },
+      -- bottom
+      -- top
+      -- telescope
+      -- dropdown
+      -- select
+      -- vertical
+      -- sidebar
+      -- telescope
+      -- ivy_split
+      -- vscode
+      layout = "select",
+      sources = {
+        notifications = {
+          layout = "dropdown",
+        },
+        explorer = {
+          layout = {
+            hidden = { "input" },
+            auto_hide = { "input" },
+            layout = {
+              position = "right",
+            },
+          },
+        },
+      },
+    },
+  },
+}
