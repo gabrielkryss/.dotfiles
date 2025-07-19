80cd73ad2124871c8b932ce72b7536966a12fd61
Gabriel Kwong <gabrielkwong971@gmail.com>
2025-07-19 02:25:33 -0700

    moved comdlet from top to center

---
+++ nvim/.config/LazyVim/lua/plugins/noice.lua
@@ -0,0 +1,53 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+        size = {
+          width = "auto",
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = "61%",
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 5,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          -- winblend = 10, -- lower = more opaque, higher = more transparent
+          winhighlight = {
+            Normal = "Normal",
+            NormalFloat = "NormalFloat",
+            -- FloatBorder = "DiagnosticInfo",
+            FloatBorder = "FloatBorder",
+          },
+        },
+      },
+    },
+    presets = {
+      command_palette = false,
+      bottom_search = false,
+    },
+    -- routes = {}, -- optional custom routes
+    -- -- optional LSP override settings
+    -- lsp = {
+    --   progress = { enabled = true },
+    --   hover = { enabled = true },
+    --   signature = { enabled = true },
+    -- },
+  },
+}
