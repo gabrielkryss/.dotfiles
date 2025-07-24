2517e63dd57aa533fab8748bbfce1bf6a5969cae
gabrielkryss <gabrielkwong971@gmail.com>
2025-07-24 09:59:19 -0700

    blank commit
    just moved some lines around

--- nvim/.config/LazyVim/lua/config/lazy.lua
+++ nvim/.config/LazyVim/lua/config/lazy.lua
@@ -88,11 +88,11 @@ require("lazy").setup({
     -- Utilities
     -- { import = "lazyvim.plugins.extras.util.chezmoi" },
     -- { import = "lazyvim.plugins.extras.util.gitui" },
+    { import = "lazyvim.plugins.extras.util.startuptime" },
     { import = "lazyvim.plugins.extras.util.dot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.octo" },
     { import = "lazyvim.plugins.extras.util.rest" },
-    { import = "lazyvim.plugins.extras.util.startuptime" },
 
     -- import/override with your plugins
     { import = "plugins" },
