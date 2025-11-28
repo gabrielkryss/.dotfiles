8a3b88ee774a11492446c5a1017085371ae8180d
Gabriel Kwong <gabrielkwong971@gmail.com>
2025-11-28 01:22:58 -0800

    updated colorscheme.lua

--- nvim/.config/LazyVim/lua/plugins/colorscheme.lua
+++ nvim/.config/LazyVim/lua/plugins/colorscheme.lua
@@ -1,4 +1,16 @@
 return {
+  -- Configure LazyVim to load colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin",
+      colorscheme = "everforest",
+      -- colorscheme = "rose-pine",
+      -- colorscheme = "gruvbox",
+      -- colorscheme = "tokyonight",
+    },
+  },
+
   -- add gruvbox
   {
     "ellisonleao/gruvbox.nvim",
@@ -23,11 +35,21 @@ return {
       -- disable_float_background = true,
     },
   },
-  { "ellisonleao/gruvbox.nvim" },
   { "olivercederborg/poimandres.nvim" },
   { "relastle/bluewery.vim" },
   { "EdenEast/nightfox.nvim" },
-  { "sainnhe/everforest" },
+  {
+    "neanias/everforest-nvim",
+    name = "everforest", -- explicitly set the module name
+    main = "everforest", -- tell Lazy.nvim the correct Lua module
+    lazy = false, -- load during startup
+    priority = 1000, -- load before other UI plugins
+    opts = {
+      background = "hard", -- "soft", "medium", or "hard"
+      transparent_background_level = 2,
+      italics = true,
+    },
+  },
   {
     "catppuccin/nvim",
     lazy = false,
@@ -36,9 +58,65 @@ return {
     opts = {
       transparent_background = true,
       term_colors = true,
+      auto_integrations = true,
       integrations = {
-        neotree = true,
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        -- dashboard = true,
+        -- neotree = true,
+        -- neotest = true,
+        -- fzf = true,
+        -- neogit = true,
+        -- dap = true,
+        -- dap_ui = true,
+        -- gitsigns = {
+        --   enabled = true,
+        --   -- align with the transparent_background option by default
+        --   transparent = false,
+        -- },
+        -- blink_cmp = {
+        --   style = "bordered",
+        -- },
+        -- snacks = {
+        --   enabled = true,
+        --   indent_scope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+        -- },
+        -- telescope = {
+        --   enabled = true,
+        -- },
+        -- octo = true,
+        -- overseer = true,
+        -- mini = {
+        --   enabled = true,
+        --   indentscope_color = "", -- catppuccin color (eg. `lavender`) Default: text
+        -- },
+        -- -- navic = {
+        -- --   enabled = false,
+        -- --   custom_bg = "NONE", -- "lualine" will set background to mantle
+        -- --   -- -- You NEED to enable highlight in nvim-navic setting or it won't work
+        -- --   -- require("nvim-navic").setup({
+        -- --   --   highlight = true,
+        -- --   -- }),
+        -- -- },
+        -- -- native_lsp = {
+        -- --   enabled = true,
+        -- --   virtual_text = {
+        -- --     errors = { "italic" },
+        -- --     hints = { "italic" },
+        -- --     warnings = { "italic" },
+        -- --     information = { "italic" },
+        -- --     ok = { "italic" },
+        -- --   },
+        -- --   underlines = {
+        -- --     errors = { "underline" },
+        -- --     hints = { "underline" },
+        -- --     warnings = { "underline" },
+        -- --     information = { "underline" },
+        -- --     ok = { "underline" },
+        -- --   },
+        -- --   inlay_hints = {
+        -- --     background = true,
+        -- --   },
+        -- -- },
+        -- -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
     },
   },
@@ -62,18 +140,6 @@ return {
       cursorline = true,
       -- transparent_background = true,
       nvim_tree_darker = true,
-    },
-  },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "catppuccin",
-      colorscheme = "everforest",
-      -- colorscheme = "rose-pine",
-      -- colorscheme = "gruvbox",
-      -- colorscheme = "tokyonight",
     },
   },
 }
